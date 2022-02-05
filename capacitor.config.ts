5b4fbe2869e809b231a978070b9d1106bd016571
IreneAlvaradoDiaz <irene220400@gmail.com>
2022-02-05 14:18:53 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'cuida-a-mi-mascota',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
